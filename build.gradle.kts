426872b4e1811d04d9ec9003dde466b5ef0283c4
Tommy Trøen <tommy.troen@nav.no>
2020-10-25 21:50:42 -0500

    ci: add ci files and plugins for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,65 +1,10 @@
-val kotlinLoggingVersion = "2.0.3"
-val logbackVersion = "1.2.3"
-val junitJupiterVersion = "5.7.0"
-val kotlinVersion = "1.4.10"
-val kotestVersion = "4.3.0"
-val jacksonVersion = "2.11.+"
-
 plugins {
-    kotlin("jvm") version "1.4.0"
-    id("org.jmailen.kotlinter") version "3.2.0"
-    id("com.github.ben-manes.versions") version "0.33.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.15"
+    id("io.codearte.nexus-staging") version "0.21.2"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_14
-    targetCompatibility = JavaVersion.VERSION_14
-    withJavadocJar()
-    withSourcesJar()
-}
-
-apply(plugin = "org.jmailen.kotlinter")
-
-repositories {
-    mavenCentral()
-    jcenter()
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
-    implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
-    testImplementation( "com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion") // for kotest framework
-    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion") // for kotest core jvm assertions
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-}
-
-tasks.javadoc {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
-}
-
-tasks {
-    withType<org.jmailen.gradle.kotlinter.tasks.LintTask> {
-        dependsOn("formatKotlin")
-    }
-
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "14"
-        }
-    }
-
-    withType<Test> {
-        useJUnitPlatform()
-    }
-
-    withType<Wrapper> {
-        gradleVersion = "6.7"
-    }
+nexusStaging {
+    username = System.getenv("SONATYPE_USERNAME")
+    password = System.getenv("SONATYPE_PASSWORD")
+    packageGroup = "no.nav"
+    delayBetweenRetriesInMillis = 5000
 }
